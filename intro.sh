a6a7fceab4cecb4722e35f0401cca30cada13516
sampath <sampathsvrk@gmail.coml>
2025-06-25 13:09:56 -0400

    using variables

--- intro.sh
+++ intro.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
 echo "Hello world"
+
+person1=phil
+person2="jay pritchett"
+
+
+echo "$person1 : Hey jay, how's it going ?"
+
+echo "$person2 : hey phil, just gearing up for my exam"
+
