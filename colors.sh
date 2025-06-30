898c8f7bdec2f7516f9d1d2ebff909f34d4ee381
sampath <sampathsvrk@gmail.coml>
2025-06-30 10:29:17 -0400

    Working on colors in shell scripting

--- colors.sh
+++ colors.sh
@@ -1,7 +1,57 @@
 #!/bin/bash
 
-echo "Hello world"
+# echo "Hello world"
 
-echo -e "\e[31mHello world\e[0m"
+# echo -e "\e[31mHello world\e[0m"
 
-echo "Let's learn shell scripting"
+# echo "Let's learn shell scripting"
+
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+
+
+USER_ID=$(id -u)
+
+if [ USER_ID -ne 0 ]
+then:
+    echo -e "$R Error: You need root access to run this script $N"
+    exit 90
+else
+    echo -e "$G You have root access to run the script $N"
+fi
+
+
+Validate(){
+
+    if [ $1 -eq 0 ]
+    then
+        echo -e "$Y$2 is already installed on your server $N"
+    else
+        echo -e "$Y$2 is unavailable on your server, let's install it $N"
+        dnf install $2 -y
+
+        if [ $1 -ne 0]
+        then
+            echo -e "$R Error: Failed to install $2 $N"
+        else
+            echo -e "$G $2 is successfully installed $N"
+        fi
+    fi
+
+}
+
+dnf list installed nginx
+
+Validate "$?" "nginx"
+
+
+dnf list installed mysql
+Validate $? "mysql"
+
+
+dnf list installed python3
+Validate $? "python3"
+
