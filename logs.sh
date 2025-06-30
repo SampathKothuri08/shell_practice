be7095f8cf418d37b21e2ba1bceaa11de6e8afa4
sampath <sampathsvrk@gmail.coml>
2025-06-30 13:01:01 -0400

    used the tee command to store the logs and print on the terminal

--- logs.sh
+++ logs.sh
@@ -13,7 +13,7 @@ LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
 
 mkdir -p $LOGS_FOLDER
 
-echo "script started executing at : $(date)" &>> $LOG_FILE
+echo "script started executing at : $(date)" | tee -a $LOG_FILE
 
 
 
@@ -21,10 +21,10 @@ USER_ID=$(id -u)
 
 if [ $USER_ID -ne 0 ]
 then
-    echo -e "$R Error: You need root access to run this script $N" &>> $LOG_FILE
+    echo -e "$R Error: You need root access to run this script $N" | tee -a $LOG_FILE
     exit 90
 else
-    echo -e "$G You have root access to run the script $N" &>> $LOG_FILE
+    echo -e "$G You have root access to run the script $N" | tee -a $LOG_FILE
 fi
 
 
@@ -32,16 +32,16 @@ Validate(){
 
     if [ $1 -eq 0 ]
     then
-        echo -e "$Y$2 is already installed on your server $N" &>> $LOG_FILE
+        echo -e "$Y$2 is already installed on your server $N" | tee -a $LOG_FILE
     else
-        echo -e "$Y$2 is unavailable on your server, let's install it $N" &>> $LOG_FILE
+        echo -e "$Y$2 is unavailable on your server, let's install it $N" | tee -a $LOG_FILE
         dnf install $2 -y &>> $LOG_FILE
 
         if [ $? -ne 0 ]
         then
-            echo -e "$R Error: Failed to install $2 $N" &>> $LOG_FILE
+            echo -e "$R Error: Failed to install $2 $N" | tee -a $LOG_FILE
         else
-            echo -e "$G $2 is successfully installed $N" &>> $LOG_FILE
+            echo -e "$G $2 is successfully installed $N" | tee -a $LOG_FILE
         fi
     fi
 
