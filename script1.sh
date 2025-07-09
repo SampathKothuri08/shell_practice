b710b8441a57a85667a138a9e55cfc0e5ca8ab73
sampath <sampathsvrk@gmail.coml>
2025-07-09 13:07:56 -0400

    Scripting

--- script1.sh
+++ script1.sh
@@ -7,10 +7,10 @@ Var=Ritz
 
 echo "The process ID of the script1.sh is $$"
 
-# chmod u+x ./script2.sh
+chmod u+x ./script2.sh
 
-# ./script2.sh
+./script2.sh
 
-source ./script2.sh
+# source ./script2.sh
 echo "$Var goes to college in the morning"
 echo "$person is a good lad"
