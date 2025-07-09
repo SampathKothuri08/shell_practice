6ff050da195c83b401add431f72350d0af2c2016
sampath <sampathsvrk@gmail.coml>
2025-07-09 12:55:04 -0400

    Scripting

---
+++ script1.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+
+echo "Hello world"
+
+Var=Ritz
+
+
+./script2.sh
+echo "$Var goes to college in the morning"
---
+++ script2.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Hey there"
+
+person=phil
+
+echo "Phil goes for a walk everyday in the morning"
