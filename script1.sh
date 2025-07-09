8d7cdd665dcc38421fd3dcd8c59ae7c66fdee53c
sampath <sampathsvrk@gmail.coml>
2025-07-09 13:04:18 -0400

    Scripting

--- script1.sh
+++ script1.sh
@@ -6,8 +6,10 @@ echo "Hello world"
 Var=Ritz
 
 
-chmod u+x ./script2.sh
+# chmod u+x ./script2.sh
 
-./script2.sh
+# ./script2.sh
+
+source ./script2.sh
 echo "$Var goes to college in the morning"
 echo "$person is a good lad"
