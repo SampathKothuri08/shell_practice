11966a3114ecccb02985e63ec087dcef768f7b15
sampath <sampathsvrk@gmail.coml>
2025-07-09 13:05:50 -0400

    Scripting

--- script1.sh
+++ script1.sh
@@ -5,6 +5,7 @@ echo "Hello world"
 
 Var=Ritz
 
+echo "The process ID of the script1.sh is $$"
 
 # chmod u+x ./script2.sh
 
--- script2.sh
+++ script2.sh
@@ -6,3 +6,4 @@ person=phil
 
 echo "Phil goes for a walk everyday in the morning"
 
+echo "The process ID of script2.sh is $$"
